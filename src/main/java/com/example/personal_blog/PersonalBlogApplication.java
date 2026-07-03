e71568b14d929695e49a529b8dd7ba9300d1ec46
吴璇 <15487104+hswl-wu-xuan@user.noreply.gitee.com>
2026-07-03 16:40:03 +0800

    第五次提交 增加了密码加密

--- src/main/java/com/example/personal_blog/PersonalBlogApplication.java
+++ src/main/java/com/example/personal_blog/PersonalBlogApplication.java
@@ -2,10 +2,11 @@ package com.example.personal_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PersonalBlogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PersonalBlogApplication.class, args);
     }
--- src/main/java/com/example/personal_blog/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/personal_blog/service/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.personal_blog.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.personal_blog.common.BusinessException;
 import com.example.personal_blog.common.ResultCode;
+import com.example.personal_blog.config.PasswordConfig;
 import com.example.personal_blog.entity.User;
 import com.example.personal_blog.mapper.UserMapper;
 import com.example.personal_blog.service.UserService;
 import com.example.personal_blog.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,13 +19,23 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     /**
      * 注册接口
      * @param user
      */
     @Override
     public void register(User user) {
+        String rawPassword = user.getPassword();
+        //  加密
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        user.setPassword(encodedPassword);
+
         user.setCreateTime(LocalDateTime.now());
+
         userMapper.insert(user);
     }
 
@@ -38,11 +50,16 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.selectOne(
                 new QueryWrapper<User>()
                         .eq("username", username)
-                        .eq("password", password)
         );
 
         if (user == null) {
             throw new BusinessException(ResultCode.UNAUTHORIZED,"用户名错误");
+        }
+        // 加密
+        boolean matches = passwordEncoder.matches(password, user.getPassword());
+
+        if(!matches){
+            throw new BusinessException(ResultCode.UNAUTHORIZED,"密码错误");
         }
 
         // 生成JWT
